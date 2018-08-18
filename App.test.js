de65075e91ef5bb84712eb4401ce6fce535678ac
Eric P. Katz <ericpkatz@gmail.com>
2018-08-18 08:22:51 -0400

    set up enzyme

--- App.test.js
+++ App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import App from './App';
+import { Text } from 'react-native';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
+  const app = shallow(<App />);
+  expect(app.find(Text).length).toEqual(3);
+  /*
   const rendered = renderer.create(<App />).toJSON();
+  console.log(rendered);
   expect(rendered).toBeTruthy();
+  */
 });
