d1f9e96cd35d63699bbdc730feb6aeb1308fab5b
Eric P. Katz <ericpkatz@gmail.com>
2018-08-19 16:52:42 -0400

    remove web maps dependency

--- App.js
+++ App.js
@@ -5,18 +5,6 @@ import Home from './src/Components/Home';
 import { Provider } from 'react-redux';
 import store from './src/store';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { App } from './App';
-import { Text } from 'react-native';
-import { shallow } from 'enzyme';
-
-it('renders without crashing', () => {
-  const app = shallow(<App />);
-  expect(app.find(Text).length).toEqual(3);
-});
